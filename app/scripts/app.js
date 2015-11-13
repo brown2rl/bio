9895be884f094317d1a0ac51cd28fe11406c1d26
Robert Brown <brown2rl@mail.uc.edu>
2015-11-13 01:37:08 -0500

    angular scaffolding

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name bioApp
+ * @description
+ * # bioApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('bioApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bioApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the bioApp
+ */
+angular.module('bioApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
